071523eeb6335762565324388b9300e40fa7f9df
MiauGit <sergiudan.radu@hotmail.com>
2015-10-09 17:20:09 +0300

    return type changed in classes ValidateCreate and DoCreateWallet

---
+++ main/inc/Types.h
@@ -0,0 +1,25 @@
+/*
+	Author: Miau
+	
+	This file contains the enum types
+	
+*/
+
+#ifndef TYPES_H
+#define TYPES_H
+
+enum Error_E
+{
+	ALL_GOOD = '1',
+	WRITE_TO_FILE = '2',
+	WALLET_ALREADY_EXISTS = '3',
+	CREATE_NAME_MISSING = '4',
+	CREATE_INITIAL_AMMOUNT_INVALID = '5',
+	PATH_DOES_NOT_EXIST = '6',
+	CREATE_WALLET_MESSAGE = '7'
+};
+
+
+
+
+#endif
