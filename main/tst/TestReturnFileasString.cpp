a726c634a0dcd153272ea099f4ff2a22c11a95a4
Andrei Martinescu <martinescuandrei@gmail.com>
2015-10-28 08:53:29 +0200

    adding new test

--- main/tst/TestReturnFileasString.cpp
+++ main/tst/TestReturnFileasString.cpp
@@ -8,8 +8,8 @@
 #include "gtest/gtest.h"
 #include "MainFunctions.h"
 #include <string>
-#include <fstream>
-#include <iostream>
+
+
 
 //Test 1 
 TEST(ReturnFileasString, NotOpen)
@@ -22,6 +22,15 @@ TEST(ReturnFileasString, NotOpen)
 TEST(ReturnFileasString, ReadContent)
 {
 	std::string actual = "default_currency = RON\ndefault_income_category = salary\ndefault_spending_category = other\ndefault_wallet \ncurrencies = RON, EUR, USD\nrate_EUR_RON = 4.42\nrate_RON_EUR = 0.23\nrate_USD_RON = 3.92\nrate_EUR_USD = 1.13";
-	EXPECT_EQ(actual, ReturnFileasString("main\\tst\\ConfigFiles\\empty_after_def_wallet.config"));
-	std::string content = ReturnFileasString("main\\tst\\ConfigFiles\\empty_after_def_wallet.config");
+	std::string expected = ReturnFileasString("main\\tst\\ConfigFiles\\empty_after_def_wallet.config");
+	EXPECT_EQ(actual, expected);
+	
+}
+//Test 3
+TEST(ReturnFileasString, ReadContentEmpty)
+{
+	std::string actual = "";
+	std::string expected = ReturnFileasString("main\\tst\\ConfigFiles\\empty_config.config");
+	EXPECT_EQ(actual,expected);
+	
 }
